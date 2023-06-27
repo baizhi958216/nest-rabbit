b7bf9f47a11361a038b4e511f76fd3173813ccb5
baizhi958216 <1475289190@qq.com>
2023-06-27 11:13:30 +0800

    feat: 模块声明全局
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/pay/pay.module.ts
+++ src/pay/pay.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PayController } from './controller/pay.contoller';
 import { PayService } from './service/pay.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule],
   controllers: [PayController],
   providers: [PayService],
 })
